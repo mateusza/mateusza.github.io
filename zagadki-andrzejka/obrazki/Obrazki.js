2a0b1f6b8c839f7187481d8684b84cd569d44119
Mateusz Adamowski <mateusz@adamowski.pl>
2020-11-01 20:35:37 +0100

    wiecej obazkow

---
+++ zagadki-andrzejka/obrazki/Obrazki.js
@@ -0,0 +1,2 @@
+var Obrazki = [
+"arbuz.png", "banan.png", "biedronka.png", "cytryna.png", "dom.png", "gruszka.png", "jablko.png", "kosciol.png", "kot.png", "krowa.png", "motyl.png", "mysz.png", "noz.png", "oko.png", "pies.png", "pomarancza.png", "rekawiczki.png", "roza.png", "samochod.png", "samolot.png", "trawa.png", "truskawka.png", "widelec.png"]
