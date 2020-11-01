3e310e793d0561c43cf501fe9314754cd00adbe0
Mateusz Adamowski <mateusz@adamowski.pl>
2020-11-01 20:39:04 +0100

    ucho

--- zagadki-andrzejka/obrazki/Obrazki.js
+++ zagadki-andrzejka/obrazki/Obrazki.js
@@ -1,2 +1,2 @@
 var Obrazki = [
-"arbuz.png", "banan.png", "biedronka.png", "cytryna.png", "dom.png", "gruszka.png", "jablko.png", "kosciol.png", "kot.png", "krowa.png", "motyl.png", "mysz.png", "noz.png", "oko.png", "pies.png", "pomarancza.png", "pralka.png", "rekawiczki.png", "roza.png", "samochod.png", "samolot.png", "trawa.png", "truskawka.png", "wanna.png", "widelec.png"]
+"arbuz.png", "banan.png", "biedronka.png", "cytryna.png", "dom.png", "gruszka.png", "jablko.png", "kosciol.png", "kot.png", "krowa.png", "motyl.png", "mysz.png", "noz.png", "oko.png", "pies.png", "pomarancza.png", "pralka.png", "rekawiczki.png", "roza.png", "samochod.png", "samolot.png", "trawa.png", "truskawka.png", "ucho.png", "wanna.png", "widelec.png"]
